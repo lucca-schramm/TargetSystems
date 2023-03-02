84318f86d0af3bbaf1525b4559e2458cbc2ffb69
Lucca Schramm <luccascho@gmail.com>
2023-03-02 15:46:24 -0300

    Subindo a solução do último exercício

--- Exercise3.py
+++ Exercise3.py
@@ -7,3 +7,22 @@ IMPORTANTE:
 a) Usar o json ou xml disponível como fonte dos dados do faturamento mensal;
 b) Podem existir dias sem faturamento, como nos finais de semana e feriados. Estes dias devem ser ignorados no cálculo da média;
 """
+import pandas as pd
+
+df= pd.read_json("dados.json")
+
+df.drop(df.loc[df['valor']<1].index, inplace=True)
+soma=df['valor'].sum()
+count=df['valor'].count()
+med=soma/count
+df_1 = (df.loc[df['valor']>med])
+x=df_1['valor'].count()
+
+min=df['valor'].min()
+max=df['valor'].max()
+
+print(f'O menor valor de faturamento foi: {min}')
+print(f'O maior valor de faturamento foi: {max}')
+print(f'A média de faturamento foi: {med}')
+
+print(f'Em {x} dias houve faturamento superior à média mensal.')
